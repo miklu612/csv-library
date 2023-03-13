34b4437e18a33afcc19693e6da4cf754fe0fd653
Robo <robo@mail>
2023-03-13 17:03:28 +0200

    Added comment to csv_reader.h

--- include/csv_reader.h
+++ include/csv_reader.h
@@ -3,6 +3,27 @@
 
 #include "csv.h"
 
+/*
+
+    CsvReader_load reads a csv file, and parses it into a 
+    Csv struct.
+
+    Arguments:
+        Csv* csv:
+            The Csv struct that the file contents will be 
+            loaded in
+
+        char* path:
+            The path to the file that you want to read
+
+    Returns:
+        False:
+            The function failed to load the file.
+
+        True:
+            The function loaded the file into the Csv struct
+
+*/
 bool CsvReader_load(Csv* csv, char* path);
 
 #endif
